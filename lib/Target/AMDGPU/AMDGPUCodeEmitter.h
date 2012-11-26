a0b4a4b86dc713e90fb3df089d9f872829d633fd
Tom Stellard <thomas.stellard@amd.com>
2012-11-26 16:53:28 +0000

    AMDGPU: Fix 4-space indentation
    Reviewed-by: Vincent Lejeune <vljn at ovi.com>

--- lib/Target/AMDGPU/AMDGPUCodeEmitter.h
+++ lib/Target/AMDGPU/AMDGPUCodeEmitter.h
@@ -16,32 +16,32 @@
 
 namespace llvm {
 
-  class AMDGPUCodeEmitter {
-  public:
-    uint64_t getBinaryCodeForInstr(const MachineInstr &MI) const;
-    virtual uint64_t getMachineOpValue(const MachineInstr &MI,
+class AMDGPUCodeEmitter {
+public:
+  uint64_t getBinaryCodeForInstr(const MachineInstr &MI) const;
+  virtual uint64_t getMachineOpValue(const MachineInstr &MI,
                                    const MachineOperand &MO) const { return 0; }
-    virtual unsigned GPR4AlignEncode(const MachineInstr  &MI,
+  virtual unsigned GPR4AlignEncode(const MachineInstr  &MI,
                                      unsigned OpNo) const {
-      return 0;
-    }
-    virtual unsigned GPR2AlignEncode(const MachineInstr &MI,
-                                     unsigned OpNo) const {
-      return 0;
-    }
-    virtual uint64_t VOPPostEncode(const MachineInstr &MI,
-                                   uint64_t Value) const {
-      return Value;
-    }
-    virtual uint64_t i32LiteralEncode(const MachineInstr &MI,
-                                      unsigned OpNo) const {
-      return 0;
-    }
-    virtual uint32_t SMRDmemriEncode(const MachineInstr &MI, unsigned OpNo)
-                                                                     const {
-      return 0;
-    }
-  };
+    return 0;
+  }
+  virtual unsigned GPR2AlignEncode(const MachineInstr &MI,
+                                   unsigned OpNo) const {
+    return 0;
+  }
+  virtual uint64_t VOPPostEncode(const MachineInstr &MI,
+                                 uint64_t Value) const {
+    return Value;
+  }
+  virtual uint64_t i32LiteralEncode(const MachineInstr &MI,
+                                    unsigned OpNo) const {
+    return 0;
+  }
+  virtual uint32_t SMRDmemriEncode(const MachineInstr &MI, unsigned OpNo)
+                                                                   const {
+    return 0;
+  }
+};
 
 } // End namespace llvm
 
--- lib/Target/AMDGPU/AMDILSIDevice.cpp
+++ lib/Target/AMDGPU/AMDILSIDevice.cpp
@@ -35,7 +35,7 @@ AMDGPUSIDevice::getGeneration() const {
 
 std::string
 AMDGPUSIDevice::getDataLayout() const {
-    return std::string("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16"
+  return std::string("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16"
       "-i32:32:32-i64:64:64-f32:32:32-f64:64:64-f80:32:32"
       "-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64"
       "-v96:128:128-v128:128:128-v192:256:256-v256:256:256"
