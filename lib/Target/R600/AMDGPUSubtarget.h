c9b4a2ff93ca0e753aa132101147d0e1c9b9f64b
Matt Arsenault <Matthew.Arsenault@amd.com>
2014-12-06 14:25:33 -0500

    XXX - R600/SI: Add stub hazard recognizer

--- lib/Target/R600/AMDGPUSubtarget.h
+++ lib/Target/R600/AMDGPUSubtarget.h
@@ -212,6 +212,16 @@ public:
     return true;
   }
 
+  bool enablePostMachineScheduler() const override {
+    return getGeneration() >= SOUTHERN_ISLANDS;
+  }
+
+  // We must always do hazard recognition because of some special cases where
+  // the hardware does not do dependency checking.
+  CodeGenOpt::Level getOptLevelToEnablePostRAScheduler() const override {
+    return CodeGenOpt::None;
+  }
+
   void overrideSchedPolicy(MachineSchedPolicy &Policy,
                            MachineInstr *begin, MachineInstr *end,
                            unsigned NumRegionInstrs) const override;
