1335fda506a963b1d238e1683f25c938c697360e
Matt Arsenault <Matthew.Arsenault@amd.com>
2015-02-23 11:36:21 -0800

    AMDGPU/SI: Fix read2 merging into a super register.
    If the read2 produced was supposed to be writing into a
    super register, it would use the wrong subregister indices.
    Fix this by inserting copies, so we only ever write to a vreg_64.
    Run the register coalescer again to clean this up, although this
    isn't ideal.
    
    Also remove the assert that offset1 > offset0.
    
    There isn't a real reason to not allow this other than a minor
    convenience in the compiler, and it doesn't seem worth the effort
    of avoiding it.

--- lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -269,6 +269,7 @@ void GCNPassConfig::addPreRegAlloc() {
     // also need extra copies to the address operand to be eliminated.
     initializeSILoadStoreOptimizerPass(*PassRegistry::getPassRegistry());
     insertPass(&MachineSchedulerID, &SILoadStoreOptimizerID);
+    insertPass(&MachineSchedulerID, &RegisterCoalescerID);
   }
   addPass(createSIShrinkInstructionsPass(), false);
   addPass(createSIFixSGPRLiveRangesPass(), false);
