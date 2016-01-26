f6b5ef00e85e3e664b5960a424c823251bd543eb
Matt Arsenault <Matthew.Arsenault@amd.com>
2016-01-25 18:29:59 -0800

    Revert "Clear TargetOptions when resetting them."
    This reverts commit ab8cbf3dc0f134e6a4f4bc8b2591919f4664d12a.

--- lib/Target/TargetMachine.cpp
+++ lib/Target/TargetMachine.cpp
@@ -62,7 +62,6 @@ TargetMachine::~TargetMachine() {
 void TargetMachine::resetTargetOptions(const Function &F) const {
 #define RESET_OPTION(X, Y)                                                     \
   do {                                                                         \
-    Options.X = false;                                                         \
     if (F.hasFnAttribute(Y))                                                   \
       Options.X = (F.getFnAttribute(Y).getValueAsString() == "true");          \
   } while (0)
